13e07375ccb9cdc7709c69e84bc017f5554eceea
Franco <lmassa.fms@gmail.com>
2023-11-29 18:23:41 -0300

    agregamos un ejemplo de setInterval y un objeto date

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 // setTimeout ejecuta una funcion pasado cierto tiempo en milisegundos
 function saludar() {
     document.write('<p>Hola Mundo 🌎</p>');
+    if(contador === 5) {
+        clearInterval(idSetInterval);
+    }
+    contador++;
 }
 
-window.setTimeout(saludar, 5000);
+// window.setTimeout(saludar, 5000);
 
-setTimeout(()=>{
-    document.write('<p>Hola Mundo nuevo 🌎</p>');
-}, 3000);
+// setTimeout(()=>{
+//     document.write('<p>Hola Mundo nuevo 🌎</p>');
+// }, 3000);
+let contador = 1;
+const idSetInterval = setInterval(saludar, 2000); // su objetivo es ejecutar una funcion en determinado tiempo
+console.log(idSetInterval)
 
-// setInterval
+const fecha = new Date();
+console.log(fecha);
+console.log(fecha.getMonth);
